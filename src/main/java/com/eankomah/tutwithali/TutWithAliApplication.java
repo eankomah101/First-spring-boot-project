948b8872e2f10e52448c8b02c612df1b8a41a724
Evans Ankomah <evans.ankomah@amalitech.org>
2023-11-27 09:35:00 +0000

    project init

---
+++ src/main/java/com/eankomah/tutwithali/TutWithAliApplication.java
@@ -0,0 +1,13 @@
+package com.eankomah.tutwithali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutWithAliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutWithAliApplication.class, args);
+	}
+
+}
